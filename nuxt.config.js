9a4f9fc80791dcdc76282ea9f2408bb521690057
optimistic <optimistic@optimisticnoMacBook-Pro.local>
2021-08-22 11:38:56 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'webtaskNuxt',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.typekit.net/baw1bhn.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css',
+        integrity: "sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p",
+        crossorigin:"anonymous"
+      },
+
+
+    ],
+    script: [
+      { src: 'scripts/libs/slide-card.js', body: true},
+      { src: 'scripts/libs/menu-btn.js', body: true},
+      { src: 'scripts/main.js', body: true },
+    ],
+  },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    // プロジェクト内の SCSS ファイル
+    '~/assets/css/vendors/bootstrap-reboot.css',
+    '~/assets/css/main.scss',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>index vue</h1>
+  </div>
+</template>
